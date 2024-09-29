a391b5872addc404d02fc981b6ef38c9b0e856ca
lopatop <pavellopato327@gmail.com>
2024-09-29 15:06:32 +0200

    function function random phrases

--- array_random.js
+++ array_random.js
@@ -1,1 +1,17 @@
 //Generate random phrases!
+
+
+function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex]
+}
+
+const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
+const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
+const words3 = ["морковку", "макароны", "косточку"];
+
+function makePhrases(param1, param2, param3) {
+    return `${getRandomElement(param1)} ${getRandomElement(param2)} ${getRandomElement(param3)}`;
+}
+
+console.log(makePhrases(words1, words2, words3));
