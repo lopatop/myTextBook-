6266c94c219e70d2938fe878cc97a42bef3dc584
lopatop <pavellopato327@gmail.com>
2024-09-30 22:11:23 +0200

    first commit

---
+++ bubble_solution.js
@@ -0,0 +1,29 @@
+const score = [60, 50, 60, 58, 54, 54,
+    58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51,
+    69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18,
+    41, 53, 55, 61, 51, 44];
+
+
+const costs = [.25, .27, .25, .25, .25, .25,
+    .33, .31, .25, .29, .27, .22,
+    .31, .25, .25, .33, .21, .25,
+    .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25,
+    .25, .25, .27, .25, .27, .29];
+
+
+
+
+//Bubble solution #0 score: 60
+//Bubble solution #1 score: 50
+//Bubble solution #2 score: 60
+//Bubbles tests: 36
+
+//Highest bubble score: 69
+//Solutions with highest score: #11, #18
+
+//Bubble Solution #11 is the most cost effective
+
+
