1d5ba3c1079fc2d35c39104cd26802625fbee6f5
lopatop <pavellopato327@gmail.com>
2024-10-13 13:17:08 +0200

    summ numbers in object

--- sum_object.js
+++ sum_object.js
@@ -8,3 +8,14 @@
 // Если объект salaries пуст, то результат должен быть 0.
 
 // Решение: 
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+}
+let result = 0;
+for (let key in salaries) {
+    result += salaries[key];
+}
+console.log(result);
