6571ce768f1b2bc986857084adf5a8f5797bed61
lopatop <pavellopato327@gmail.com>
2024-10-13 13:10:21 +0200

    function multyply numeric

--- multiplication_object_values.js
+++ multiplication_object_values.js
@@ -10,3 +10,18 @@
 
 // Решение:
 
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu",
+}
+
+function multiplyNumeric(menu) {
+    for (let key in menu) {
+        if (typeof menu[key] === 'number') {
+            menu[key] *= 2;
+        }
+    }
+}
+
+multiplyNumeric(menu);
