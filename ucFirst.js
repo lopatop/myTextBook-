b1ae97b25b6f4471f1f19715e22b38b3b3dde102
lopatop <pavellopato327@gmail.com>
2024-10-27 17:36:36 +0100

    function ucFirst

--- ucFirst.js
+++ ucFirst.js
@@ -1,3 +1,4 @@
 // Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
 // ucFirst("вася") == "Вася";
 
+const ucFirst = str => str[0].toUpperCase() + str.slice(1);
